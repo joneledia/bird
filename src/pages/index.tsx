f6e2b320497a9c289d16c1666ee061ee585238c9
joneledia <eledia.jon@gmail.com>
2023-03-27 13:57:57 +0800

    Add profile picture with and user fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,68 @@
 import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
-
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 import { api } from "~/utils/api";
+import type { RouterOutputs } from "~/utils/api";
+import Image from "next/image";
+
+dayjs.extend(relativeTime);
+
+const CreatePostWizard = () => {
+  const { user } = useUser();
+
+  console.log(user);
+  if (!user) return null;
+
+  return (
+    <div className="flex w-full gap-4 ">
+      <Image
+        src={user.profileImageUrl}
+        alt="Profile image"
+        className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
+      />
+      <input
+        placeholder="Type something here..."
+        className="grow bg-transparent outline-none"
+      />
+    </div>
+  );
+};
+
+type PostWithUser = RouterOutputs["posts"]["getAll"][number];
+const PostView = (props: PostWithUser) => {
+  const { post, author } = props;
+
+  return (
+    <div key={post.id} className=" flex gap-3 border-b border-slate-400 p-8">
+      <Image
+        src={author.profileImageUrl}
+        className="h-14 w-14 rounded-full"
+        alt={`@${author.username}'s profile picture`}
+        width={56}
+        height={56}
+      />
+      <div className="flex flex-col">
+        <div className="flex gap-1 font-bold text-slate-300">
+          <span>{`@${author.username}`}</span>
+          <span className="font-thin">{` · ${dayjs(
+            post.createdAt
+          ).fromNow()}`}</span>
+        </div>
+      </div>
+      <span>{post.content}</span>
+    </div>
+  );
+};
 
 const Home: NextPage = () => {
   const user = useUser();
-  const { data } = api.posts.getAll.useQuery();
+  const { data, isLoading } = api.posts.getAll.useQuery();
+  if (isLoading) return <div> Loading...</div>;
+  if (!data) return <div>Something went wrong</div>;
   return (
     <>
       <Head>
@@ -23,11 +78,11 @@ const Home: NextPage = () => {
                 <SignInButton />
               </div>
             )}
-            {!!user.isSignedIn && <SignOutButton />}
+            {!!user.isSignedIn && <CreatePostWizard />}
           </div>
-          <div>
-            {data?.map((post) => (
-              <div key={post.id}> {post.content}</div>
+          <div className="flex flex-col">
+            {[...data, ...data]?.map((fullPost) => (
+              <PostView {...fullPost} key={fullPost.post.id} />
             ))}
           </div>
         </div>
